17974083de7cd2a498ab122fec5462a270d7816c
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-11-18 04:56:43 -0800

    add all args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    allnum = 0
+    for i in range(len(sys.argv) - 1):
+        allnum += int(sys.argv[i + 1])
+    print("{}".format(allnum))
