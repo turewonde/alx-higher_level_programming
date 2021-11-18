8c3a3866aaa493fcfc65472fa1d9702e38807eac
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-11-18 04:48:00 -0800

    print number and arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif length == 1:
         print("1 argument:")
-    else :
+    else:
         print("{} arguments:".format(length))
     for i in range(length):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
