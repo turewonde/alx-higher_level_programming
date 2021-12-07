fef18bb45361f4b3922c34ee6ef4286071108e34
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-12-07 05:53:06 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """ Represent a square."""
+
+    def __init__(self, size):
+        """Square with size"""
+
+        self.__size = size  #using __size, becuse of private instance attributes
