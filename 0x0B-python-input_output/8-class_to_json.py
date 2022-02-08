a464a85f93cb00fafd472241a03dc528a874d65d
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 05:09:45 -0800

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-"""Defines a JSON file-reading function."""
-import json
+"""json object"""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+def class_to_json(obj):
+    """dict obj"""
+    return obj.__dict__
