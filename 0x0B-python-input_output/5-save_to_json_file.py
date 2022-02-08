e36eb3ba2eb73ee4bb1ea90513d90fb49c5ee578
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 05:04:29 -0800

    Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# 5-to_json_string.py
-"""Defines a string-to-JSON function."""
+"""save to json"""
 import json
 
 
-def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+def save_to_json_file(my_obj, filename):
+    """save to json"""
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(json.dumps(my_obj))
