9d63d43f4f17b9908ce65c0e2fcf4e486a9925c3
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 04:52:21 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 # 1-number_of_lines.py
-"""Defines a text file line-counting function."""
-
-
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)
