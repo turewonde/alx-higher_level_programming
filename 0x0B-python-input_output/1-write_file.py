9c62f038f92fe7a8ebc55a26467435b46f185742
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 04:55:25 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
+"""write file"""
+
+
 def write_file(filename="", text=""):
+    """"write file"""
     with open(filename, mode="w", encoding="utf-8") as fd:
         fd.write(text)
     return len(text)
