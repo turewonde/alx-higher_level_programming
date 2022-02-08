9b684974ddf24da3ca3fbc89a1b402fabe5d61aa
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 05:18:38 -0800

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
-# 12-student.py
-"""Defines a class Student."""
+""" pascal triangle"""
 
 
-class Student:
-    """Represent a student."""
+def pascal_triangle(n=4500):
+    """print pascal"""
+    pascal = [[0]*i for i in range(1, n+1)]
+    cmpt = 0
+    for i in range(n):
+        pascal[i][0] = 1
+        pascal[i][-1] = 1
+        for j in range(0, i//2):
+            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
+            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    return pascal
