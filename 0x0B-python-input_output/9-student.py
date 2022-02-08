c607e09dc38cfa0daf37b62bef6cca81e659bce1
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-02-08 05:11:20 -0800

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-# 9-add_item.py
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""class student"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to_json"""
+        return self.__dict__
